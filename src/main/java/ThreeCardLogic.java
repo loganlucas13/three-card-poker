23edeed94834e16613b8d68b5267ac3856ea0ad6
JJonKang <jjonkang03@gmail.com>
2024-11-06 01:53:58 -0600

    Did work on evalHand

--- src/main/java/ThreeCardLogic.java
+++ src/main/java/ThreeCardLogic.java
@@ -1,4 +1,7 @@
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.Collections;
 
 public class ThreeCardLogic {
 
@@ -10,8 +13,38 @@ public class ThreeCardLogic {
     // 4 - flush
     // 5 - pair
     public static int evalHand(ArrayList<Card> hand) {
-        // TODO: implement
-        return -1;
+    	Set<Integer> sameV = new HashSet<>();
+    	Set<Character> sameS = new HashSet<>();
+    	int res = 0;
+    	int valueCount = 1;
+    	int suitCount = 1;
+        for(Card card : hand) {
+        	int value = card.getValue();
+        	char suit = card.getSuit();
+        	if(sameV.contains(value)) valueCount += 1;
+        	else sameV.add(value);
+        	if(sameS.contains(suit)) suitCount += 1;
+        	else sameS.add(suit);
+        }
+        int straightFlush = 0;
+        if(valueCount == 2) res = 5;
+        int length = 1;
+        for(int value : sameV) {
+            if(!sameV.contains(value - 1)) {
+                while(sameV.contains(value + length)) length++;
+            }
+        }
+        if(length == 3) {
+        	res = 4;
+        	straightFlush += 1;
+        }
+        if(suitCount == 3) {
+        	res = 3;
+        	straightFlush += 1;
+        }
+        if(valueCount == 3) res = 2;
+        if(straightFlush == 2) res = 1;
+        return res;
     }
 
 
