e08a967b3623f607494a424ad8728c0243ad70de
Logan Lucas <logs10658@gmail.com>
2024-11-09 04:58:54 -0600

    modified screen size and updated monologue

--- src/main/java/JavaFXTemplate.java
+++ src/main/java/JavaFXTemplate.java
@@ -17,7 +17,7 @@ public class JavaFXTemplate extends Application {
 	// this should be allowed based on Piazza post @389, which says
 	// "You can define the Dealer and Player's in your controller if that is what you are using to really drive your game,
 	// instead of having them be in the JavaFXTemplate but not using them."
-	// i put these here to be extra safe, i'm sorry for the confusion
+	// i put these here to be extra safe, i'm sorry for the confusion!
 	Player playerOne = new Player();
 	Player playerTwo = new Player();
 	Dealer theDealer = new Dealer();
@@ -30,7 +30,7 @@ public class JavaFXTemplate extends Application {
 	public void start(Stage primaryStage) throws Exception {
 		Parent root = FXMLLoader.load(getClass().getResource("FXML/menu.fxml"));
 
-		Scene scene = new Scene(root, 1250, 1000);
+		Scene scene = new Scene(root, 1280, 1024);
 		scene.setFill(null); // so scene background doesn't show when switching scenes
 
 		primaryStage.setScene(scene);
